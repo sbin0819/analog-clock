99bf974afd087acf620717a1ba9406253e6fa9b6
sbin <sbinha123@gmail.com>
2024-03-07 12:31:18 +0900

    chore: 코드 정리

--- src/components/Clock/Clock.Second.tsx
+++ src/components/Clock/Clock.Second.tsx
@@ -2,6 +2,7 @@ import { useClockStore } from '../../store';
 
 const Second = () => {
   const { secondsRotation } = useClockStore((state) => state);
+
   return (
     <div
       className="absolute w-1 h-24 rounded-md bg-red-600 transform origin-bottom"
